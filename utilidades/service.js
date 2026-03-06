aa5a581e3c9e8672a2fea37905ab63c9c32af012
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2026-03-06 10:01:16 -0500

    fix export service

--- utilidades/service.js
+++ utilidades/service.js
@@ -3,7 +3,7 @@ const URL_APP = 'https://script.google.com/macros/s/AKfycbwR-tNcUVVhiOV24UphHCrq
 const MODO_CONSULTA = "consulta";
 const LUGAR_EVENTO = "casa";
 
-async function consultaAsistente(identification) {
+export async function consultaAsistente(identification) {
     const urlConsulta = `${URL_APP}?1051066918=${encodeURIComponent(identification)}&modo=${encodeURIComponent(MODO_CONSULTA)}&lugar=${encodeURIComponent(LUGAR_EVENTO)}`;
     try {
         const respuesta = await fetch(urlConsulta);
